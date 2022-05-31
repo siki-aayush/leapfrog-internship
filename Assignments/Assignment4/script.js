b40df3ccb1c8eccda46adb159e2b4d751d4f1594
siki <musyajuaayush@gmail.com>
2022-05-31 19:15:08 +0545

    adds assignment 4

---
+++ Assignments/Assignment4/script.js
@@ -0,0 +1,26 @@
+const hamBtn = document.querySelector(".hamburger");
+const hamImg = hamBtn.getElementsByTagName("img")[0];
+const rightNav = document.querySelector(".navbar__right");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+console.log("hamImg", hamImg)
+
+function toggle(event) {
+    console.log("testing");
+
+    if (!rightNav.classList.contains("nav__disp")) {
+        hamImg.src = "./assets/images/icons/menu.png";
+        rightNav.classList.add("nav__disp")
+        main.style.display = "block";
+        footer.style.display = "block";
+
+      } else {
+        hamImg.src = "./assets/images/icons/cross.png";
+        rightNav.classList.remove("nav__disp")
+        main.style.display = "none";
+        footer.style.display = "none";
+      }
+
+}
+
+hamBtn.addEventListener("click", toggle)
